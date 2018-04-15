c0a6849ff00c332828448c42fe9178325d5d6a57
Michael Keller <mk@viu.ch>
2018-04-15 09:56:39 +0200

    Reinsert camera

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('babel-polyfill');
 require('webrtc-adapter');
 
 var Instascan = {
-  Scanner: require('./src/scanner')
+  Scanner: require('./src/scanner'),
+  Camera: require('./src/camera')
 };
 
 module.exports = Instascan;
